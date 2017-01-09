adc63ae825c561f0d4f094a696a2fc58f1d5fbce
Shadi Akiki <shadiakiki1986@gmail.com>
2017-01-09 08:52:21 +0200

    fixup

--- exporter/main.php
+++ exporter/main.php
@@ -17,6 +17,7 @@ if(!$repo) {
 }
 
 // iterate over databases in odbc
+$result = [];
     foreach($ini as $name=>$value) {
       if($verbose) {
         echo "Copying locks to $name".PHP_EOL;
@@ -34,3 +35,19 @@ $repo->commit('a new commit message');
 // tag
 // no need to tag since relying on commit dates
 // $repo->tag(date('Y-m-d H:M:i'));
+
+//###########################
+
+// get diff and send result by email (this is parallel to the UI)
+$commits = $repo->commits();
+$head_1=array_shift($commits);
+$head_2=array_shift($commits);
+$difference = $repo->diff($head_1,$head_2);
+$difference = json_decode($difference,true);
+
+// format to html
+$report = new Report($difference);
+
+// send by email
+$emailer = new Emailer($report);
+$emailer->send(['my@email.com']);
