63ca0625f228845d650e36f466d3e4547414ff15
Shadi Akiki <shadiakiki1986@gmail.com>
2017-01-10 18:51:36 +0200

    converting differences array into html using twig

--- exporter/src/DeepDiffObject.php
+++ exporter/src/DeepDiffObject.php
@@ -4,12 +4,38 @@ namespace PdoGit;
 
 class DeepDiffObject {
 
-  function __construct(array $differences) {
+  function __construct(array $differences, \DateTime $d1, \DateTime $d2) {
     $this->differences = $differences;
+    $this->d1=$d1;
+    $this->d2=$d2;
+  }
+
+  // filter differences
+  private function split(string $type) {
+    return array_filter(
+      $this->differences,
+      function($entry) use($type) {
+        return $entry['type']==$type;
+      }
+    );
   }
 
   public function html()
   {
+    $loader = new \Twig_Loader_Filesystem(__DIR__.'../twig');
+    $twig = new \Twig_Environment($loader)
+
+    $sqlt = $twig->render(
+      "differences.html.twig",
+      array(
+        'edited'=>$this->split('E'),
+        'new'=>$this->split('N'),
+        'deleted'=>$this->split('D'),
+        'd1'=>$d1->format('Y-m-d'),
+        'd2'=>$d2->format('Y-m-d')
+      )
+    );
+
   }
 
   public function email()
